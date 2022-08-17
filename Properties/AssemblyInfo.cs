79fbc0bbf98b42d621a5157717260614e33c6eff
Kevin Costa <kevinvtc@outlook.com>
2022-08-17 17:26:07 -0300

    Fix
    - Now the program will always run in elevation mode (admin). It is necessary for executing outside of OOBE SYSTEM privileges.
    -  The program now officially targets Windows 10 and newer OSes.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
 // usando o "*" como mostrado abaixo:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.2208")]
-[assembly: AssemblyFileVersion("1.1.0.2208")]
+[assembly: AssemblyVersion("1.1.1.2208")]
+[assembly: AssemblyFileVersion("1.1.1.2208")]
